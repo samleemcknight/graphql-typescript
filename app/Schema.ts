9bacbca47b7e75be3f5f66b2bd243e552d1d2291
Sam McKnight <sam.mcknight@equipmentshare.com>
2022-10-17 19:54:40 -0600

    initialize repo

---
+++ app/Schema.ts
@@ -0,0 +1,15 @@
+import { gql } from "apollo-server-express";
+
+const Schema = gql`
+    type Person {
+        id: ID!
+        name: String
+    }
+
+    type Query {
+        getAllPeople: [Person]
+        getPerson(id: Int): Person
+    }
+`;
+
+export default Schema;
